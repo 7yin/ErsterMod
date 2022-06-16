94152b7f3f2972e45851f66572bae35669c9bb0e
7yin <isusik2006@gmail.com>
2022-06-16 15:46:24 +0200

    Raw Tanzanite and Tanzanite added with German and English Translation

--- src/main/java/net/elias/tutorialmod/TutorialMod.java
+++ src/main/java/net/elias/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.elias.tutorialmod;
 
+import net.elias.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,5 +12,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerModItems();
 	}
 }
